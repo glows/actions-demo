114cbf10ec24e3a5776067f832302a718c5db9dd
Steven <duncyun@gmail.com>
2020-07-04 21:20:53 +0800

    change eslint

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import App from '../App';
-import router from './router.js'
+// import router from './router.js'
 import Detail from '../pages/Detail';
 import About from '../components/About'
 import MyCanvas from '../pages/MyCanvas';
